c4df66d8620da54e0a0489502268ade178fded4e
Tarun Janapati <63157544+74run@users.noreply.github.com>
2024-04-28 16:29:50 -0500

    Update index.js

--- my-node-backend/index.js
+++ my-node-backend/index.js
@@ -35,7 +35,11 @@ const app = express();
 const port = process.env.REACT_APP_BACKPORT;
 
 
-app.use(cors());
+app.use(cors({
+  origin: ["https://boron-atom-chi.vercel.app"],
+  methods: ["POST", "GET"],
+  credentials: true
+}));
 app.use(bodyParser.json());
 
 
