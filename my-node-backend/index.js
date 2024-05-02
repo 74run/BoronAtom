1dbfced6c9cccacb21b9b4d5319f1b6413d0d263
Tarun Janapati <63157544+74run@users.noreply.github.com>
2024-05-02 18:50:30 -0500

    Update index.js

--- my-node-backend/index.js
+++ my-node-backend/index.js
@@ -37,12 +37,14 @@ const port = 3001;
 
 
 
-app.use(cors({
-  origin: "https://boron-atom-one.vercel.app",
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true
-}));
+const corsOptions = {
+  origin: 'https://boron-atom-one.vercel.app', // Explicitly allow your frontend domain
+  methods: 'GET,POST,PUT,DELETE', // Specify allowed methods as needed
+  credentials: true, // If your frontend needs to send cookies or credentials with the request
+  allowedHeaders: 'Content-Type,Authorization', // Specify allowed headers
+};
 
+app.use(cors(corsOptions));
 
 
 app.use(bodyParser.json());
@@ -95,6 +97,11 @@ app.use('/api/userprofile', Google);
 app.use('/run', (req,res)=> {
   res.send("server is running")
 })
+
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello from the backend!' });
+});
 
 
 
