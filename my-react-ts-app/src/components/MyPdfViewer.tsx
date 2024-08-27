e8e78efeb2900df22152e70681e815936c6966da
Tarun Janapati <63157544+74run@users.noreply.github.com>
2024-08-27 15:32:52 -0500

    Update MyPdfViewer.tsx

--- my-react-ts-app/src/components/MyPdfViewer.tsx
+++ my-react-ts-app/src/components/MyPdfViewer.tsx
@@ -153,12 +153,8 @@ const previewPdf = async () => {
       return;
     }
 
-
-    // Wait for the state to update fully (you can use an effect with a dependency on `eduDetails` if needed)
+    // Wait for the state to update fully
     await new Promise(resolve => setTimeout(resolve, 300));
-
-
-    // Continue only if `eduDetails` is populate
 
     const educations = (eduDetails?.education || []).filter(education => education.includeInResume);
     const experiences = (eduDetails?.experience || []).filter(experience => experience.includeInResume);
@@ -169,235 +165,164 @@ const previewPdf = async () => {
     const skills = (eduDetails?.skills || []).filter(skill => skill.includeInResume);
     const contacts = eduDetails?.contact || [];
 
-       const summarySection = summarys.map(
-        (summary)=>`
-        \\textit{${summary.content}}
-        `
-      ).join("\n");
-  
-      const projectSection = projects.map((project) => `
-      \\project{${project.name}}{${project.skills}}{${project.startDate.month}/${project.startDate.year} -- ${project.endDate.month}/${project.endDate.year}}{
-          \\begin{bullet-list-minor}
-              ${convertToLatex(project.description.split('*').slice(1).map((part, index) => `\\item ${part.trim()}`).join('\n'))}
-          \\end{bullet-list-minor}
-      }
-  `).join("\n");
-  
-    
-      const educationSection = educations.map(
-        (education) => `
+    const summarySection = summarys.length > 0 ? `
+      \\header{Summary}
+      ${summarys.map(summary => `\\textit{${summary.content}}`).join("\n")}
+    ` : '';
+
+    const educationSection = educations.length > 0 ? `
+      \\header{Education}
+      ${educations.map(education => `
         \\school{${education.university}}{${education.degree}}{Graduation: ${education.endDate.month}/${education.endDate.year}}{\\textit{${education.major} \\labelitemi GPA: ${education.cgpa}}}
-        `
-      ).join("\n");
-  
-      const experienceSection = experiences.map((experience) => {
-        // Check if description exists and is not empty
-        if (experience.description && experience.description.trim() !== '') {
-          return `
-            \\employer{${experience.jobTitle}}{--${experience.company}}{${experience.startDate.month}/${experience.startDate.year} -- ${experience.endDate.month}/${experience.endDate.year}}{${experience.location}}
-            \\begin{bullet-list-minor}
-                ${convertToLatex(experience.description.split('*').slice(1).map((part, index) => `\\item ${part.trim()}`).join('\n'))}
-            \\end{bullet-list-minor}
-          `;
-        } else {
-          // Return other fields without modification if description is empty
-          return `
-            \\employer{${experience.jobTitle}}{--${experience.company}}{${experience.startDate.month}/{${experience.startDate.year} -- ${experience.endDate.month}}/${experience.endDate.year}}{${experience.location}}
-          `;
+      `).join("\n")}
+    ` : '';
+
+    const experienceSection = experiences.length > 0 ? `
+      \\header{Experience}
+      ${experiences.map(experience => `
+        \\employer{${experience.jobTitle}}{--${experience.company}}{${experience.startDate.month}/${experience.startDate.year} -- ${experience.endDate.month}/${experience.endDate.year}}{${experience.location}}
+        ${experience.description && experience.description.trim() !== '' ? `
+        \\begin{bullet-list-minor}
+          ${convertToLatex(experience.description.split('*').slice(1).map(part => `\\item ${part.trim()}`).join('\n'))}
+        \\end{bullet-list-minor}` : ''}
+      `).join("\n")}
+    ` : '';
+
+    const projectSection = projects.length > 0 ? `
+      \\header{Projects}
+      ${projects.map(project => `
+        \\project{${project.name}}{${project.skills}}{${project.startDate.month}/${project.startDate.year} -- ${project.endDate.month}/${project.endDate.year}}{
+          \\begin{bullet-list-minor}
+            ${convertToLatex(project.description.split('*').slice(1).map(part => `\\item ${part.trim()}`).join('\n'))}
+          \\end{bullet-list-minor}
         }
-      }).join("\n");
-      
-  
-  
-  
-      const involvementSection = involvements.map(
-        (involvement) => `
-            \\begin{bullet-list-major}
-            \\item \\textbf{${involvement.role}} \\labelitemi ${involvement.organization} \\hfill ${involvement.startDate.month}/${involvement.startDate.year} -- ${involvement.endDate.month}/${involvement.endDate.year}
-            ${involvement.description.split('*').slice(1).map((part, index) => `\\newline -{${part}}`).join('')}
-            \\end{bullet-list-major}
-            `
-      ).join("\n");
-      
-  
-      const certificationSection = certifications.map(
-        (certification) => `
+      `).join("\n")}
+    ` : '';
+
+    const certificationSection = certifications.length > 0 ? `
+      \\header{Certifications}
+      ${certifications.map(certification => `
         \\begin{bullet-list-major}
-        \\item \\textbf{${certification.name}} \\labelitemi ${certification.issuedBy} \\hfill ${certification.issuedDate.month}/${certification.issuedDate.year} -- ${certification.expirationDate.month}/${certification.expirationDate.year}
+          \\item \\textbf{${certification.name}} \\labelitemi ${certification.issuedBy} \\hfill ${certification.issuedDate.month}/${certification.issuedDate.year} -- ${certification.expirationDate.month}/${certification.expirationDate.year}
         \\end{bullet-list-major}
-        `
-      )
-      .join("\n");
+      `).join("\n")}
+    ` : '';
 
-      const skillSection = skills.map(
-        (skill) => `
+    const involvementSection = involvements.length > 0 ? `
+      \\header{Involvements}
+      ${involvements.map(involvement => `
         \\begin{bullet-list-major}
-        \\item \\textbf{${skill.domain}:} ${skill.name}
+          \\item \\textbf{${involvement.role}} \\labelitemi ${involvement.organization} \\hfill ${involvement.startDate.month}/${involvement.startDate.year} -- ${involvement.endDate.month}/${involvement.endDate.year}
+          ${involvement.description.split('*').slice(1).map(part => `\\newline -{${part}}`).join('')}
         \\end{bullet-list-major}
-        `
-      )
-      .join("\n");
-  
+      `).join("\n")}
+    ` : '';
 
-      
-    
-    // Usage:
-    
-  
-  
-  
-  
-    
-      // Add similar sections for projects, certifications, skills, involvements
-    
-      const latexCode = `
-      \\documentclass{article}
-      \\usepackage{geometry}
-      
-      \\usepackage{lipsum}
+    const skillSection = skills.length > 0 ? `
+      \\header{Skills}
+      ${skills.map(skill => `
+        \\begin{bullet-list-major}
+          \\item \\textbf{${skill.domain}:} ${skill.name}
+        \\end{bullet-list-major}
+      `).join("\n")}
+    ` : '';
 
-      % Set margins for the first page
-      \\newgeometry{top=0.5in, bottom=1in, left=1in, right=1in}
-      \\usepackage{enumitem}
-      \\setlist{nosep}
-      \\usepackage{hyperref}
-      \\usepackage{fancyhdr}
-      \\pagestyle{fancy}
-      \\renewcommand{\\headrulewidth}{0pt}
-      \\rfoot{Page \\thepage}
-      \\usepackage{fontawesome}
-  
-  
-      \\setlength{\\textheight}{9.5in}
-      \\setlength{\\footskip}{30pt} 
-      \\pagestyle{fancy}
-      \\raggedright
-      \\fancyhf{}
-      \\renewcommand{\\headrulewidth}{0pt}
-  
-      \\setlength{\\hoffset}{-2pt}
-      \\setlength{\\footskip}{30pt}
-  
-      \\def\\bull{\\vrule height 0.8ex width .7ex depth -.1ex }
-  
-      \\newcommand{\\contact}[3]{
+    // Construct the LaTeX code with only sections that have content
+    const latexCode = `
+    \\documentclass{article}
+    \\usepackage{geometry}
+    \\usepackage{lipsum}
+    % Set margins for the first page
+    \\newgeometry{top=0.5in, bottom=1in, left=1in, right=1in}
+    \\usepackage{enumitem}
+    \\setlist{nosep}
+    \\usepackage{hyperref}
+    \\usepackage{fancyhdr}
+    \\pagestyle{fancy}
+    \\renewcommand{\\headrulewidth}{0pt}
+    \\rfoot{Page \\thepage}
+    \\usepackage{fontawesome}
+    \\setlength{\\textheight}{9.5in}
+    \\setlength{\\footskip}{30pt} 
+    \\pagestyle{fancy}
+    \\raggedright
+    \\fancyhf{}
+    \\renewcommand{\\headrulewidth}{0pt}
+    \\setlength{\\hoffset}{-2pt}
+    \\setlength{\\footskip}{30pt}
+    \\def\\bull{\\vrule height 0.8ex width .7ex depth -.1ex }
+
+    \\newcommand{\\contact}[3]{
       \\vspace*{5pt}
       \\begin{center}
-      {\\LARGE \\scshape {#1}}\\\\
-      \\vspace{3pt}
-      #2 
-      \\vspace{2pt}
-      #3
+        {\\LARGE \\scshape {#1}}\\\\
+        \\vspace{3pt}
+        #2 
+        \\vspace{2pt}
+        #3
       \\end{center}
       \\vspace*{-8pt}
-      }
-     
-      \\newcommand{\\school}[4]{
-        \\textbf{#1} \\labelitemi #2 \\hfill #3 \\\\ #4 \\vspace*{5pt}
-      }
-  
-      \\newcommand{\\employer}[4]{{
-        \\vspace*{2pt}%
-        \\textbf{#1} #2 \\hfill #3\\\\ #4 \\vspace*{2pt}}
-        }
-  
-      \\newcommand{\\project}[4]{{
-          \\vspace*{2pt}% 
-          \\textbf{#1} #2 \\hfill #3\\\\ #4 \\vspace*{2pt}}
-          }
-          
-      \\newcommand{\\lineunder}{
-        \\vspace*{-8pt} \\\\ \\hspace*{-18pt} 
-        \\hrulefill \\\\
-        }
-  
-      \\newcommand{\\header}[1]{{
-        \\hspace*{-15pt}\\vspace*{6pt} \\textsc{#1}} \\vspace*{-6pt} 
-        \\lineunder
-        }
-  
-      \\newcommand{\\content}{
-          \\vspace*{2pt}%
-          }
-  
-      \\renewcommand{\\labelitemi}{
-        $\\vcenter{\\hbox{\\tiny$\\bullet$}}$\\hspace*{3pt}
-        }
-        
-        \\renewcommand{\\labelitemii}{
-        $\\vcenter{\\hbox{\\tiny$\\bullet$}}$\\hspace*{-3pt}
-        }
-  
-  
-        \\newenvironment{bullet-list-major}{
-          \\begin{list}{\\labelitemii}{\\setlength\\leftmargin{3pt} 
-          \\topsep 0pt \\itemsep -2pt}}{\\vspace*{4pt}\\end{list}
-          }
-  
-        \\newenvironment{bullet-list-minor}{
-          \\begin{list}{\\labelitemii}{\\setlength\\leftmargin{15pt} 
-            \\topsep 0pt \\itemsep -2pt}}{\\vspace*{4pt}\\end{list}
-            }
-  
-  
-      \\begin{document}
-  
-      \\small
-      \\smallskip
-      \\vspace*{-44pt}
-  
-      \\begin{center}
-        {\\LARGE \\textbf{${
-          contacts[0]?.name // Check if contacts[0].name exists
-              ? contacts[0].name // If it exists, use it
-              : `${userDetails?.firstName} ${userDetails?.lastName}` // If not, fallback to userDetails?.firstName
-      }}} \\\\
-        \\faPhone\\ ${contacts[0]?.phoneNumber} \\quad
-        \\faEnvelope\\ \\href{mailto:  ${
-          contacts[0]?.email
-          ? contacts[0].email
-          : `${userDetails?.email}`
-      }}{  ${
-        contacts[0]?.email
-        ? contacts[0].email
-        : `${userDetails?.email}`
-    }} \\quad
-        \\faLinkedin\\ \\url{${contacts[0]?.linkedIn}}
-      \\end{center}
-     \\vspace*{4pt}%
-      \\header{Summary}
-  
-      {${summarySection}}
-  
-      \\vspace*{4pt}%
-  
-     \\header{Education}
-  
-      {${educationSection}}
-  
-      \\vspace*{4pt}%
-      \\header{Experience}
-  
-      {${experienceSection}}
-      \\vspace*{4pt}%
-
-      \\header{Skills}
-      {${skillSection}}
-  
-      \\vspace*{4pt}%
-      \\header{Projects}
-      {${projectSection}}
-  
-      \\vspace*{4pt}%
-      \\header{Certifications}
-      {${certificationSection}}
-      \\vspace*{4pt}%
-      \\header{Involvements}
-      {${involvementSection}}
-      \\end{document}
+    }
+    \\newcommand{\\school}[4]{
+      \\textbf{#1} \\labelitemi #2 \\hfill #3 \\\\ #4 \\vspace*{5pt}
+    }
+    \\newcommand{\\employer}[4]{{
+      \\vspace*{2pt}%
+      \\textbf{#1} #2 \\hfill #3\\\\ #4 \\vspace*{2pt}}
+    }
+    \\newcommand{\\project}[4]{{
+      \\vspace*{2pt}% 
+      \\textbf{#1} #2 \\hfill #3\\\\ #4 \\vspace*{2pt}}
+    }
+    \\newcommand{\\lineunder}{
+      \\vspace*{-8pt} \\\\ \\hspace*{-18pt} 
+      \\hrulefill \\\\
+    }
+    \\newcommand{\\header}[1]{{
+      \\hspace*{-15pt}\\vspace*{6pt} \\textsc{#1}} \\vspace*{-6pt} 
+      \\lineunder
+    }
+    \\newcommand{\\content}{
+      \\vspace*{2pt}%
+    }
+    \\renewcommand{\\labelitemi}{
+      $\\vcenter{\\hbox{\\tiny$\\bullet$}}$\\hspace*{3pt}
+    }
+    \\renewcommand{\\labelitemii}{
+      $\\vcenter{\\hbox{\\tiny$\\bullet$}}$\\hspace*{-3pt}
+    }
+    \\newenvironment{bullet-list-major}{
+      \\begin{list}{\\labelitemii}{\\setlength\\leftmargin{3pt} 
+      \\topsep 0pt \\itemsep -2pt}}{\\vspace*{4pt}\\end{list}
+    }
+    \\newenvironment{bullet-list-minor}{
+      \\begin{list}{\\labelitemii}{\\setlength\\leftmargin{15pt} 
+      \\topsep 0pt \\itemsep -2pt}}{\\vspace*{4pt}\\end{list}
+    }
+    \\begin{document}
+    \\small
+    \\smallskip
+    \\vspace*{-44pt}
+    \\begin{center}
+      {\\LARGE \\textbf{${
+        contacts[0]?.name ? contacts[0].name : `${userDetails?.firstName} ${userDetails?.lastName}`
+    }}} \\\\
+      \\faPhone\\ ${contacts[0]?.phoneNumber} \\quad
+      \\faEnvelope\\ \\href{mailto:${contacts[0]?.email ? contacts[0].email : `${userDetails?.email}`}}{
+        ${contacts[0]?.email ? contacts[0].email : `${userDetails?.email}`}
+    } \\quad
+      \\faLinkedin\\ \\url{${contacts[0]?.linkedIn}}
+    \\end{center}
+    ${summarySection}
+    ${educationSection}
+    ${experienceSection}
+    ${skillSection}
+    ${projectSection}
+    ${certificationSection}
+    ${involvementSection}
+    \\end{document}
     `;
 
-    
     // Step 3: Store the LaTeX code on your server
     await axios.post(`${process.env.REACT_APP_API_URL}/store-latex`, { latexCode });
 
